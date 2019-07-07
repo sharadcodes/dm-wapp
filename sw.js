b6773633642c707b75983a12ec63e5745666b7b2
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2019-07-07 16:37:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,13 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
-       '/'
+       './',
+       './icon/',
+       './icon',
+       './main.js',
+       './main.css',
+       './index.js',      
+       './index.html'
      ]);
    })
  );
