80354ea220a16c75be468811be06f480aac98109
sharadcodes <mauryablog@gmail.com>
2019-10-02 07:03:13 +0530

    New ui

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
+var flag = true;
+
+function reply_click(clicked_id) {
+  if (
+    clicked_id != "backspace" &&
+    document.getElementById("c-no").innerText.length <= 14
+  ) {
+    if (flag === true) {
+      flag = false;
+      document.getElementById("c-no").innerText = "";
+      document.getElementById("c-no").innerText += clicked_id;
+    } else {
+      document.getElementById("c-no").innerText += clicked_id;
+    }
+  }
+  if (clicked_id == "backspace") {
+    var number = "" + document.getElementById("c-no").innerText;
+    var new_number = number.substring(0, number.length - 1);
+
+    document.getElementById("c-no").innerText = new_number;
+  }
+}
+
+
 function sendDM() {
-    var dm_input_num = document.getElementById('dm-in').value;
-    window.location.href = "https://api.whatsapp.com/send?phone=" + dm_input_num;
+  var dm_input_num = document.getElementById("c-no").innerText;
+  window.location.href = "https://api.whatsapp.com/send?phone=" + dm_input_num;
 }
